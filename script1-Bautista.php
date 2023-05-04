245b8c9a8fa18574797d844a5b72f8241e67c74b
jbauseb <132004225+jbauseb@users.noreply.github.com>
2023-05-04 21:07:19 +0200

    Add files via upload

---
+++ script1-Bautista.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Script1 para la Tarea 05 - DAW. 
+ * Incluye cuatro funciones matemáticas (sumar, restar, multiplicar y dividir).
+ *
+ * @author Profesorado
+ * @author José Alfredo Bautista Sebastiao
+ * @version 1.0
+ * 
+ * */
+ 
+ /**
+  * Función sumar. Realiza la suma de dos números enteros.
+  * @version 1.0
+  * @param int $num1 Primer sumando
+  * @param int $num2 Segundo sumando
+  * @return int Suma
+  */
+function sumar($num1, $num2) {
+  return $num1 + $num2;
+}
+
+/**
+  * Función restar. Realiza la resta de dos números enteros.
+  * @version 1.0
+  * @param int $num1 Minuendo
+  * @param int $num2 Sustraendo
+  * @return int Resta
+  */
+function restar($num1, $num2) {
+  return $num1 - $num2;
+}
+
+/**
+  * Función multiplicar. Realiza la multiplicación de dos números enteros.
+  * @version 1.0
+  * @param int $num1 Primer factor
+  * @param int $num2 Segundo factor
+  * @return int Producto
+  */
+function multiplicar($num1, $num2) {
+  return $num1 * $num2;
+}
+
+/**
+  * Función dividir. Realiza la división de dos números enteros.
+  * @version 1.0
+  * @param int $num1 Dividendo
+  * @param int $num2 Divisor
+  * @return float División  
+  * @throws Excepción si el divisor es 0.
+  */
+function dividir($num1, $num2) {
+  if ($num2 == 0) {
+    return "Error: No se puede dividir entre cero.";
+  } else {
+    return $num1 / $num2;
+  }
+}
+
+?>
---
+++ script2-Bautista.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Script2 para la Tarea 05 - DAW. Incluye la Clase Calculadora.
+ * @author Profesorado
+ * @author José A. Bautista Sebastiao
+ * @version 1.0
+ * @package Application
+ *
+ */
+ 
+ /**
+ * Clase Calculadora. Incluye cuatro funciones para realizar cálculos matemáticos: suma, resta, multiplicación y división.
+ * @version 1.0
+ *
+ */
+class Calculadora {
+  
+  /**
+  * Función sumar. Realiza la suma de dos números enteros.
+  * @version 1.0
+  * @param int $num1 Primer sumando
+  * @param int $num2 Segundo sumando
+  * @return int Resultado
+  */
+  public static function sumar($num1, $num2) {
+    return $num1 + $num2;
+  }
+
+/*
+  * Función restar. Realiza la resta de dos números enteros.
+  * @version 1.0
+  * @param int $num1 Minuendo
+  * @param int $num2 Sustraendo
+  * @return int Resta
+  */
+  public static function restar($num1, $num2) {
+    return $num1 - $num2;
+  }
+
+/**
+  * Función multiplicar. Realiza la multiplicación de dos números enteros.
+  * @version 1.0
+  * @param int $num1 Primer factor
+  * @param int $num2 Segundo factor
+  * @return int Producto
+  */
+  public static function multiplicar($num1, $num2) {
+    return $num1 * $num2;
+  }
+
+/**
+  * Función dividir. Realiza la división de dos númeroes enteros.
+  * @version 1.0
+  * @param int $num1 Dividendo
+  * @param int $num2 Divisor
+  * @return float División  
+  * @throws Excepción si el divisor es 0.
+  */
+  public static function dividir($num1, $num2) {
+    if ($num2 == 0) {
+      return "Error: No se puede dividir entre cero.";
+    } else {
+      return $num1 / $num2;
+    }
+  }
+  
+}
+
+?>
